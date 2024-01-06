2fc8bfccb777e01cb8650b19fd51e47a111ed5a6
Tushar Kanta Swain <tusharoxacular@gmail.com>
2024-01-06 10:48:34 +0530

    adding files

---
+++ src/controllers/auth/Logout.js
@@ -0,0 +1,21 @@
+// Logout
+const Logout = async (req, res) => {
+  try {
+    // cleraing the token from cookie
+    await res.clearCookie("entertainment_app_token");
+    return res
+      .status(200)
+      .json({
+        statusCode: 200,
+        Status: "Success",
+        msg: "Successfully Logout.",
+      });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ statusCode: 500, success: "false", msg: error.message });
+  }
+};
+
+module.exports = Logout;
