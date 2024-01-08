a2a7e22bc21a6143386c5ab6cb4089376e5cc8f6
Tushar Kanta Swain <143312816+tusharOxacular09@users.noreply.github.com>
2024-01-08 11:41:54 +0530

    Update Login.js

--- src/controllers/auth/Login.js
+++ src/controllers/auth/Login.js
@@ -23,14 +23,19 @@ const Login = async (req, res) => {
           } else if (response) {
             // creating session and token
             const userEmail = userFromDB["email"];
-            const token = jwt.sign({ userEmail }, process.env.JWT_KEY, {
-              expiresIn: "1d",
+            const token = jwt.sign({ email: userEmail }, process.env.JWT_KEY, {
+              expiresIn: "2h",
             });
-            // setting up the cookie
-            res.cookie("entertainment_app_token", token, { sameSite: 'None', secure: true });
-            res
-              .status(200)
-              .json({ statusCode: 200, data: {"entertainment_app_token": token }, msg: "User LoggedIn Successfully" });
+            
+            // send token in user cookie
+            const options = {
+              expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+              httpOnly: true,
+              domain: 'entertainment-app-server.onrender.com',
+              secure: true
+            }
+            // { sameSite: 'None', secure: true }
+            res.status(200).cookie("entertainment_app_token", token, options).json({ statusCode: 200, data: {"entertainment_app_token": token }, msg: "User LoggedIn Successfully" });
           } else {
             return res
               .status(201)
